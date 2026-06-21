d90569cf3f375e003d9c7e36e50f434311f0fd3f
Jishnu Raj <jica.etzc@gmail.com>
2026-06-21 08:50:02 +0530

    fix(android): expose runtime model API

--- android/BachataS4/core/runtime/build.gradle.kts
+++ android/BachataS4/core/runtime/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:model"))
+    api(project(":core:model"))
     api(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.serialization.json)
